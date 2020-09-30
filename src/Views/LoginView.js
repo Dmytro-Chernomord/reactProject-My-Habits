910d802a4e20d064d78946c15c85c9799ec401d9
Nataliia <n3v3st4@gmail.com>
2020-09-30 21:29:20 +0300

    removes buttons on homepage

--- src/Views/LoginView.js
+++ src/Views/LoginView.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import LoginForm from '../components/LogIn/LoginForm';
 import RegisterForm from '../components/LogIn/RegisterForm';
 import Main from '../components/LogIn/Main';
-import MainPrivateView from '../Views/PrivateViews/MainPrivateView';
 const styles = {
   display: 'flex',
 };
@@ -14,7 +13,6 @@ export default class LoginView extends Component {
         <LoginForm />
         <Main />
         <RegisterForm />
-        <MainPrivateView />
       </div>
     );
   }
