fdea81ea8284e50b2a9a323d94f8b1653896c0a3
ihor <malishkoio@gmail.com>
2020-09-30 20:33:21 +0300

    up data home page

--- src/Views/HomeView.js
+++ src/Views/HomeView.js
@@ -1,27 +1,61 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import authOperation from '../redux/auth/authOperation';
 import TempBut from '../components/TempBut';
+import NotificationsPage from '../components/NotificationsPage/NotificationsPage';
+import AchievementsPage from '../components/AchievementsPage/AchievementsPage';
+import CheckListPage from '../components/CheckListPage/CheckListPage';
 
 const styles = {
   display: 'flex',
   box: { outline: '1px solid teal' },
+  title: { backgroundColor: 'gray' },
 };
 
 class HomeView extends Component {
   render() {
+    const { match } = this.props;
     return (
       <>
         <h2>Это домашняя страница за логиненого пользователя</h2>
         <div style={styles}>
           <div style={styles.box}>
             LeftSideBar
+            <ul>
+              <li>
+                <Link to={`${match.url}/NotificationsPage`}>
+                  NotificationsPage
+                </Link>
+              </li>
+              <li>
+                <Link to={`${match.url}/AchievementsPage`}>
+                  AchievementsPage
+                </Link>
+              </li>
+              <li>
+                <Link to={`${match.url}/CheckListPage`}>CheckListPage</Link>
+              </li>
+            </ul>
             <TempBut onClick={this.props.onLogOut}>Log Out</TempBut>
           </div>
 
           <div style={styles.box}>
-            <header>title</header>
-            <div>Основная часть компонента CheckListPage</div>
+            <header style={styles.title}>title</header>
+            <div>
+              <Route
+                path={`${match.path}/CheckListPage`}
+                component={CheckListPage}
+              />
+              <Route
+                path={`${match.path}/NotificationsPage`}
+                component={NotificationsPage}
+              />
+              <Route
+                path={`${match.path}/AchievementsPage`}
+                component={AchievementsPage}
+              />
+            </div>
           </div>
           <div style={styles.box}>RightSideBar</div>
         </div>
---
+++ src/components/CheckListPage/CheckListPage.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class CheckListPage extends Component {
+  render() {
+    return <div>CheckListPage</div>;
+  }
+}
