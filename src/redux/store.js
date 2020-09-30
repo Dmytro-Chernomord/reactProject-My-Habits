acac7a53239c2f7fa6e7d92133567c01ea2a4045
Dmytro <tschernomord@gmail.com>
2020-09-30 17:46:21 +0200

    add rootReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,7 +11,7 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import authReducer from '../redux/auth/authReducer';
-import userData from '../redux/user/userReducer';
+import userData from '../redux/RootReducer';
 
 const authPersistConfig = {
   key: 'auth',
