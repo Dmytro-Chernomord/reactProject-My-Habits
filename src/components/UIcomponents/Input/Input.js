1814918788877230c4468156b4ae4a7db5393a60
Dmytro <tschernomord@gmail.com>
2020-10-01 14:16:36 +0200

    Update Input.js

--- src/components/UIcomponents/Input/Input.js
+++ src/components/UIcomponents/Input/Input.js
@@ -1,39 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// // import classNames from 'classnames';
 
-import './Input.module.css';
+// import './Input.module.css';
 
-const Input = ({ id, className, label, error, ...attrs }) => {
-  const classes = classNames('input', className, { error });
+// const Input = ({ id, className, label, error, ...attrs }) => {
+//   // const classes = classNames('input', className, { error });
 
-  return (
-    <div className="inputWrapper">
-      <div className="labelsWrapper">
-        {label && (
-          <label className="inputLabel" htmlFor={id}>
-            {label}
-          </label>
-        )}
-        {attrs.required && <span className="inputRequired">Required</span>}
-      </div>
-      <input name={id} id={id} className={classes} {...attrs} />
-      {error && <span className="inputError">{error}</span>}
-    </div>
-  );
-};
+//   return (
+//     <div className="inputWrapper">
+//       <div className="labelsWrapper">
+//         {label && (
+//           <label className="inputLabel" htmlFor={id}>
+//             {label}
+//           </label>
+//         )}
+//         {attrs.required && <span className="inputRequired">Required</span>}
+//       </div>
+//       <input name={id} id={id} className={classes} {...attrs} />
+//       {error && <span className="inputError">{error}</span>}
+//     </div>
+//   );
+// };
 
-Input.propTypes = {
-  id: PropTypes.string.isRequired,
-  className: PropTypes.string,
-  label: PropTypes.string,
-  error: PropTypes.string,
-};
+// Input.propTypes = {
+//   id: PropTypes.string.isRequired,
+//   className: PropTypes.string,
+//   label: PropTypes.string,
+//   error: PropTypes.string,
+// };
 
-Input.defaultProps = {
-  className: '',
-  label: '',
-  error: '',
-};
+// Input.defaultProps = {
+//   className: '',
+//   label: '',
+//   error: '',
+// };
 
-export default Input;
+// export default Input;
