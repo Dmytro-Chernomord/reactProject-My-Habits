4d3db48a421b7170d86db676f3331cce66f6e5e9
Shevchenko Elena <elena.kadru@gmail.com>
2020-10-02 20:37:56 +0300

    added new calendar style and logic

--- src/components/RightSideBar/HabitsList/HabitsList.js
+++ src/components/RightSideBar/HabitsList/HabitsList.js
@@ -7,14 +7,24 @@ import HabitsListItem from '../HabitsListItem/HabitsListItem';
 import slideStyles from './slide.module.css';
 import styles from './HabitsList.module.css';
 
+const MS_PER_DAY = 1000 * 60 * 60 * 24;
+
 export function HabitsList() {
   const habits = useSelector(state => habitsSelector.getFilterHabits(state));
 
+  const sortByTime = (a, b) => {
+    const aDay = new Date(b.planningTime).valueOf() % MS_PER_DAY;
+    const bDay = new Date(a.planningTime).valueOf() % MS_PER_DAY;
+    return bDay - aDay;
+  };
+
+  const sortedHabits = [...habits].sort(sortByTime);
+
   return (
     <TransitionGroup component="ul" className={styles.List}>
-      {habits.map(habit => (
+      {sortedHabits.map(habit => (
         <CSSTransition
-          in={habits.length > 0}
+          in={sortedHabits.length > 0}
           key={habit._id}
           timeout={250}
           classNames={slideStyles}
