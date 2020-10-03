15f83541df61f7ceed4231e3544b08f622c2b4fd
Dmytro <tschernomord@gmail.com>
2020-10-03 17:12:32 +0200

    swiper

--- src/redux/user/userSelector.js
+++ src/redux/user/userSelector.js
@@ -4,7 +4,7 @@ const getUser = state => state.user;
 const getHabits = state => state.user.habits;
 const getAvatar = state => state.user.avatar;
 const getSubscription = state => state.user.subscription;
-const getCreditCard = state => state.payment;
+const getCreditCard = state => state.payments;
 
 export default {
   getHabits,
