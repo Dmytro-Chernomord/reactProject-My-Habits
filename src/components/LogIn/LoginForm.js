358431b8d5ec39abefffaa61a392ae836f4f2ef1
Dmytro <tschernomord@gmail.com>
2020-10-03 21:13:33 +0200

    add useState

--- src/components/LogIn/LoginForm.js
+++ src/components/LogIn/LoginForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import authOperations from '../../redux/auth/authOperation';
@@ -43,47 +43,47 @@ export default function LoginForm() {
 
       <form onSubmit={handleSubmit(onSubmit)} className={styles.LoginForm}>
         <div>
-        {errors.email && errors.email.type === 'required' && (
-          <p>*oбязательное поле ввода</p>
-        )}
-        {errors.email && errors.email.type === 'pattern' && (
-          <p>{errors.email.message}</p>
-        )}
-      </div>
+          {errors.email && errors.email.type === 'required' && (
+            <p>*oбязательное поле ввода</p>
+          )}
+          {errors.email && errors.email.type === 'pattern' && (
+            <p>{errors.email.message}</p>
+          )}
+        </div>
         <div className={styles.LoginInputForm}>
           <p className={styles.LoginInputTxt}>E-mail</p>
-         <input
- className={styles.LoginInput}
-placeholder="Введите свой E-mail"
-          name="email"
-          type="email"
-          style={{
-            outlineColor: errors.email ? '#fe6083' : '#43d190',
-            borderColor: errors.email ? '#fe6083' : '#e0e0e0',
-          }}
-          ref={register({
-            required: true,
-            pattern: {
-              value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]+$/i,
-              message: '*e-mail не валидный, пример: email@mail.com',
-            },
-          })}
-        />
+          <input
+            className={styles.LoginInput}
+            placeholder="Введите свой E-mail"
+            name="email"
+            type="email"
+            style={{
+              outlineColor: errors.email ? '#fe6083' : '#43d190',
+              borderColor: errors.email ? '#fe6083' : '#e0e0e0',
+            }}
+            ref={register({
+              required: true,
+              pattern: {
+                value: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]+$/i,
+                message: '*e-mail не валидный, пример: email@mail.com',
+              },
+            })}
+          />
         </div>
-               <div>
-        {errors.password && errors.password.type === 'minLength' && (
-          <p>{errors.password.message}</p>
-        )}
-        {errors.password && errors.password.type === 'pattern' && (
-          <p>{errors.password.message}</p>
-        )}
-        {errors.password && errors.password.type === 'required' && (
-          <p>*oбязательное поле ввода</p>
-        )}
-        {errors.password && errors.password.type === 'maxLength' && (
-          <p>{errors.password.message}</p>
-        )}
-      </div>
+        <div>
+          {errors.password && errors.password.type === 'minLength' && (
+            <p>{errors.password.message}</p>
+          )}
+          {errors.password && errors.password.type === 'pattern' && (
+            <p>{errors.password.message}</p>
+          )}
+          {errors.password && errors.password.type === 'required' && (
+            <p>*oбязательное поле ввода</p>
+          )}
+          {errors.password && errors.password.type === 'maxLength' && (
+            <p>{errors.password.message}</p>
+          )}
+        </div>
         <div className={styles.LoginInputForm}>
           <p className={styles.LoginInputTxt}>Пароль</p>
           <label className={styles.LoginPassword}>
@@ -92,31 +92,31 @@ placeholder="Введите свой E-mail"
             </div>
 
             <input
-className={styles.LoginInput}
-placeholder="Введите пароль"
-          name="password"
-          type="password"
-          style={{
-            outlineColor: errors.password ? '#fe6083' : '#43d190',
-            borderColor: errors.password ? '#fe6083' : '#e0e0e0',
-          }}
-          ref={register({
-            pattern: {
-              value: /[A-Za-z0-9]$/i,
-              message:
-                '*пароль может содержать только латинские буквы и/или цифры, он не должен содержать пробелы и знаки препинания',
-            },
-            minLength: {
-              value: 8,
-              message: '*пароль должен содержать не менее 8 символов',
-            },
-            maxLength: {
-              value: 16,
-              message: '*пароль может содержать не более 16 символов',
-            },
-            required: true,
-          })}
-        />
+              className={styles.LoginInput}
+              placeholder="Введите пароль"
+              name="password"
+              type="password"
+              style={{
+                outlineColor: errors.password ? '#fe6083' : '#43d190',
+                borderColor: errors.password ? '#fe6083' : '#e0e0e0',
+              }}
+              ref={register({
+                pattern: {
+                  value: /[A-Za-z0-9]$/i,
+                  message:
+                    '*пароль может содержать только латинские буквы и/или цифры, он не должен содержать пробелы и знаки препинания',
+                },
+                minLength: {
+                  value: 8,
+                  message: '*пароль должен содержать не менее 8 символов',
+                },
+                maxLength: {
+                  value: 16,
+                  message: '*пароль может содержать не более 16 символов',
+                },
+                required: true,
+              })}
+            />
           </label>
         </div>
         <div className={styles.LoginButtonBlock}>
