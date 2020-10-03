8ef48c4f875f22938472e148823323de6cfc4f7a
Nicholas Trunov <anteycu@gmail.com>
2020-10-03 16:01:18 +0300

    add economyMoney logic

--- src/components/LeftSideBar/TimeMoney/TimeMoney.js
+++ src/components/LeftSideBar/TimeMoney/TimeMoney.js
@@ -6,6 +6,44 @@ import styles from './TimeMoney.module.css';
 
 function TimeMoney() {
   const { quiz } = useSelector(state => state);
+  const state = useSelector(state => state);
+  console.log(state);
+
+  function cigMoneyPerDay(cigPackPrice, cigPerDay) {
+    const cigPerPack = 20;
+    const cigPrice = cigPackPrice / cigPerPack;
+    const result = cigPrice * cigPerDay;
+    return result;
+  }
+
+  console.log(cigMoneyPerDay(quiz.cigarettePackPrice, quiz.cigarettePerDay));
+
+  const getFiltredCig = cigArr => {
+    return cigArr.filter(cigArrItem => cigArrItem);
+  };
+  console.log(getFiltredCig(state.cigarettes.data));
+  const filtredArr = getFiltredCig(state.cigarettes.data);
+
+  const moneyEconomy = filtredArr => {
+    const sumOfEconomyMoney = filtredArr.reduce((acc, arrItem) => {
+      const staticCigMoney = cigMoneyPerDay(
+        quiz.cigarettePackPrice,
+        quiz.cigarettePerDay,
+      );
+      const dynamicCigMoney = cigMoneyPerDay(quiz.cigarettePackPrice, arrItem);
+      const result = staticCigMoney - dynamicCigMoney;
+      console.log(`arrItem ${arrItem}`);
+      console.log(`staticCigMoney ${staticCigMoney}`);
+      console.log(`dynamicCigMoney ${dynamicCigMoney}`);
+      console.log(`result ${result}`);
+      console.log(`acc ${acc}`);
+      return acc + result;
+    }, 0);
+    return sumOfEconomyMoney;
+  };
+
+  console.log(moneyEconomy(filtredArr));
+
   return (
     <ul className={styles.container}>
       <li className={styles.timeMoneyBox}>
