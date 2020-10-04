163623e60230d801dc2c37a126174aa341897c37
Koval <dmytrokovalskyi@yahoo.com>
2020-10-04 16:39:43 +0300

    notifications remove

--- src/components/NotificationsPage/NotificationsPage.js
+++ src/components/NotificationsPage/NotificationsPage.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import styles from './notification.module.css';
 import habitSelector from '../../redux/habits/habitsSelector';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 export default function Notifications() {
+  const [isVisible, setIsVisible] = useState(false);
+  const handleClick = () => {
+    setIsVisible(true);
+  };
   const filteredHabitsData = useSelector(state =>
-    habitSelector.getFilterHabits(state),
+    habitSelector.getFilterTodayHabits(state),
   );
-  console.log(filteredHabitsData);
+
   const habitsData = useSelector(state => state.habits);
 
-  // console.log(habitsData);
   const habits = habitsData.map(el => el.data);
 
   const activeDays = habits.map(el =>
     el.filter(elm => typeof elm === 'boolean'),
   );
   const daysLeft = habits.map(el => el.filter(elm => elm === null));
-  console.log(daysLeft);
-  const youHaveThreeDaysLeft = daysLeft.filter(el => el.length === 3);
-  const youHaveFiveDaysLeft = daysLeft.filter(el => el.length === 5);
-  const halfWayTrough = daysLeft.filter(el => el.length === 10);
+  const youHaveThreeDaysLeft = daysLeft.some(el => el.length === 3);
+  const youHaveFiveDaysLeft = daysLeft.some(el => el.length === 5);
+  const halfWayTrough = daysLeft.some(el => el.length === 10);
+
   const presentActiveDays = activeDays.filter(elm => elm.length > 0);
 
   const uncompletedDays = presentActiveDays.map(el =>
@@ -36,54 +40,78 @@ export default function Notifications() {
 
   return (
     <div className={styles.container}>
-      {youHaveThreeDaysLeft && (
-        <div className={styles.box}>
-          <h2 className={styles.title}>Осталось совсем немного!</h2>
-          <p className={styles.text}>
-            Осталось 3 дня чтобы завершить привычку!
-          </p>
-        </div>
-      )}
-      {halfWayTrough && (
-        <div className={styles.box}>
-          <h2 className={styles.title}>Поздравляем! Вы на половине пути!</h2>
-          <p className={styles.text}>Большая часть позади!</p>
-        </div>
-      )}
-
-      {youHaveFiveDaysLeft && (
-        <div className={styles.box}>
-          <h2 className={styles.title}>Осталось совсем немного!</h2>
-          <p className={styles.text}>
-            Осталось 5 дня чтобы завершить привычку!
-          </p>
-        </div>
-      )}
-      {youHaveFiveDaysLeft && (
-        <div className={styles.box}>
-          <h2 className={styles.title}>Соберись!!</h2>
-          <p className={styles.text}>
-            Не забывай выполнять свои привычки!Я знаю у тебя получится!
-          </p>
-        </div>
-      )}
-
-      {youGotAchievment && (
-        <div className={styles.box}>
-          <h2 className={styles.title}>Привычка успешно освоена!</h2>
-          <p className={styles.text}>
-            Поздравляем! Вы успешно освоили свою привычку
-          </p>
-        </div>
-      )}
-      {oneDayLeft && (
-        <div className={styles.box}>
-          <h2 className={styles.title}>Ура!!! Остался один день.</h2>
-          <p className={styles.text}>
-            Остался один день чтобы завершить привычку!
-          </p>
-        </div>
-      )}
+      <TransitionGroup>
+        {youHaveThreeDaysLeft && !isVisible && (
+          <CSSTransition classNames="option" timeout={250} unmountOnExit>
+            <div onClick={handleClick} className={styles.box}>
+              <h2 className={styles.title}>Осталось совсем немного!</h2>
+              <p className={styles.text}>
+                Осталось 3 дня чтобы завершить привычку!
+              </p>
+            </div>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+      <TransitionGroup>
+        {halfWayTrough && !isVisible && (
+          <CSSTransition classNames="option" timeout={250} unmountOnExit>
+            <div onClick={handleClick} className={styles.box}>
+              <h2 className={styles.title}>
+                Поздравляем! Вы на половине пути!
+              </h2>
+              <p className={styles.text}>Большая часть позади!</p>
+            </div>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+      <TransitionGroup>
+        {youHaveFiveDaysLeft && !isVisible && (
+          <CSSTransition classNames="option" timeout={250} unmountOnExit>
+            <div onClick={handleClick} className={styles.box}>
+              <h2 className={styles.title}>Осталось совсем немного!</h2>
+              <p className={styles.text}>
+                Осталось 5 дня чтобы завершить привычку!
+              </p>
+            </div>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+      <TransitionGroup>
+        {youCanDoBetter && !isVisible && (
+          <CSSTransition classNames="option" timeout={250} unmountOnExit>
+            <div onClick={handleClick} className={styles.box}>
+              <h2 className={styles.title}>Соберись!!</h2>
+              <p className={styles.text}>
+                Не забывай выполнять свои привычки!Я знаю у тебя получится!
+              </p>
+            </div>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+      <TransitionGroup>
+        {youGotAchievment && !isVisible && (
+          <CSSTransition classNames="option" timeout={250} unmountOnExit>
+            <div onClick={handleClick} className={styles.box}>
+              <h2 className={styles.title}>Привычка успешно освоена!</h2>
+              <p className={styles.text}>
+                Поздравляем! Вы успешно освоили свою привычку
+              </p>
+            </div>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+      <TransitionGroup>
+        {oneDayLeft && !isVisible && (
+          <CSSTransition classNames="option" timeout={250} unmountOnExit>
+            <div onClick={handleClick} className={styles.box}>
+              <h2 className={styles.title}>Ура!!! Остался один день.</h2>
+              <p className={styles.text}>
+                Остался один день чтобы завершить привычку!
+              </p>
+            </div>
+          </CSSTransition>
+        )}
+      </TransitionGroup>
     </div>
   );
 }
