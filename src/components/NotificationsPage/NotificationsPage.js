26f208bc5ddf185853bf0c060523b89ea98049df
Koval <dmytrokovalskyi@yahoo.com>
2020-10-05 13:08:33 +0300

    Update NotificationsPage.js

--- src/components/NotificationsPage/NotificationsPage.js
+++ src/components/NotificationsPage/NotificationsPage.js
@@ -6,23 +6,42 @@ import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import './transition.css';
 
 export default function Notifications() {
-  const [isVisible, setIsVisible] = useState(false);
-  const handleClick = () => {
-    setIsVisible(true);
+  const [isVisibleCompleted, setIsVisibleCompleted] = useState(false);
+  const handleClickCompleted = () => {
+    setIsVisibleCompleted(true);
+  };
+  const [isVisibleHalfWay, setIsVisibleHalfWay] = useState(false);
+  const handleClickHalfWay = () => {
+    setIsVisibleHalfWay(true);
+  };
+  const [isVisibleThreeDays, setIsVisibleThreeDays] = useState(false);
+  const handleClickThreeDays = () => {
+    setIsVisibleThreeDays(true);
+  };
+  const [isVisibleFiveDays, setIsVisibleFiveDays] = useState(false);
+  const handleClickFiveDays = () => {
+    setIsVisibleFiveDays(true);
+  };
+  const [isVisibleOneDay, setIsVisibleOneDay] = useState(false);
+  const handleClickOneDay = () => {
+    setIsVisibleOneDay(true);
+  };
+  const [isVisibleReminder, setIsVisibleReminder] = useState(false);
+  const handleClickReminder = () => {
+    setIsVisibleReminder(true);
   };
   const filteredHabitsData = useSelector(state =>
     habitSelector.getFilterTodayHabits(state),
   );
-
   const habitsData = useSelector(state => state.habits);
 
-  const habits = habitsData.map(el => el.data);
+  const habits = filteredHabitsData.map(el => el.data);
 
-  const activeDays = habits.map(el =>
-    el.filter(elm => typeof elm !== 'boolean'),
+  const activeDays = habits.filter(el =>
+    el.filter(elm => typeof elm === 'boolean'),
   );
-
   console.log(activeDays);
+
   const daysLeft = habits.map(el => el.filter(elm => elm === null));
   const youHaveThreeDaysLeft = daysLeft.some(el => el.length === 3);
   const youHaveFiveDaysLeft = daysLeft.some(el => el.length === 5);
@@ -33,20 +52,20 @@ export default function Notifications() {
   const uncompletedDays = presentActiveDays.map(el =>
     el.filter(elm => elm === false),
   );
-  const youCanDoBetter = uncompletedDays.map(el => el.length > 2);
+  const youCanDoBetter = uncompletedDays.some(el => el.length > 7);
   const successfullDays = presentActiveDays.map(el =>
     el.filter(elm => elm === true),
   );
   const oneDayLeft = daysLeft.some(el => el.length === 1);
 
   const youGotAchievment = successfullDays.some(el => el.length > 20);
-
+  console.log(youGotAchievment);
   return (
     <div className={styles.container}>
       <TransitionGroup>
-        {youHaveThreeDaysLeft && !isVisible && (
+        {youHaveThreeDaysLeft && !isVisibleThreeDays && (
           <CSSTransition classNames="option" timeout={250} unmountOnExit>
-            <div onClick={handleClick} className={styles.box}>
+            <div onClick={handleClickThreeDays} className={styles.box}>
               <h2 className={styles.title}>Осталось совсем немного!</h2>
               <p className={styles.text}>
                 Осталось 3 дня чтобы завершить привычку!
@@ -55,9 +74,9 @@ export default function Notifications() {
           </CSSTransition>
         )}
 
-        {halfWayTrough && !isVisible && (
+        {halfWayTrough && !isVisibleHalfWay && (
           <CSSTransition classNames="option" timeout={250} unmountOnExit>
-            <div onClick={handleClick} className={styles.box}>
+            <div onClick={handleClickHalfWay} className={styles.box}>
               <h2 className={styles.title}>
                 Поздравляем! Вы на половине пути!
               </h2>
@@ -66,9 +85,9 @@ export default function Notifications() {
           </CSSTransition>
         )}
 
-        {youHaveFiveDaysLeft && !isVisible && (
+        {youHaveFiveDaysLeft && !isVisibleFiveDays && (
           <CSSTransition classNames="option" timeout={250} unmountOnExit>
-            <div onClick={handleClick} className={styles.box}>
+            <div onClick={handleClickFiveDays} className={styles.box}>
               <h2 className={styles.title}>Осталось совсем немного!</h2>
               <p className={styles.text}>
                 Осталось 5 дня чтобы завершить привычку!
@@ -77,9 +96,9 @@ export default function Notifications() {
           </CSSTransition>
         )}
 
-        {!isVisible && youCanDoBetter && (
+        {youCanDoBetter && !isVisibleReminder && (
           <CSSTransition classNames="option" timeout={250} unmountOnExit>
-            <div onClick={handleClick} className={styles.box}>
+            <div onClick={handleClickReminder} className={styles.box}>
               <h2 className={styles.title}>Не сдавайся!</h2>
               <p className={styles.text}>
                 Не забывай отмечать свои привычки!У тебя получится!
@@ -88,9 +107,9 @@ export default function Notifications() {
           </CSSTransition>
         )}
 
-        {youGotAchievment && !isVisible && (
-          <CSSTransition classNames="option" timeout={250} unmountOnExit>
-            <div onClick={handleClick} className={styles.box}>
+        {youGotAchievment && !isVisibleCompleted && (
+          <CSSTransition classNames="option" timeout={250}>
+            <div onClick={handleClickCompleted} className={styles.box}>
               <h2 className={styles.title}>Привычка успешно освоена!</h2>
               <p className={styles.text}>
                 Поздравляем! Вы успешно освоили свою привычку
@@ -99,9 +118,9 @@ export default function Notifications() {
           </CSSTransition>
         )}
 
-        {oneDayLeft && !isVisible && (
+        {oneDayLeft && !isVisibleOneDay && (
           <CSSTransition classNames="option" timeout={250} unmountOnExit>
-            <div onClick={handleClick} className={styles.box}>
+            <div onClick={handleClickOneDay} className={styles.box}>
               <h2 className={styles.title}>Ура!!! Остался один день.</h2>
               <p className={styles.text}>
                 Остался один день чтобы завершить привычку!
