6c60b62498c929f80871fc8a09a6e7666fd38ff4
Nicholas Trunov <anteycu@gmail.com>
2020-10-05 12:46:28 +0300

    add sinc with ProfilePage, Subscription; add home route on logo

--- src/components/LeftSideBar/LogoBox/LogoBox.js
+++ src/components/LeftSideBar/LogoBox/LogoBox.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../../images/logo/logo-black.svg';
 import styles from './logoBox.module.css';
 
@@ -6,7 +7,9 @@ export default function LogoBox() {
   return (
     <div className={styles.container}>
       <div className={styles.leftLogoSvg}>
-        <Logo />
+        <Link to={'/home'}>
+          <Logo />
+        </Link>
       </div>
     </div>
   );
