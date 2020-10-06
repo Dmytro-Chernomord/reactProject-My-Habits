5ee347b80a74cc1f3532b54d847871d5cdf91318
Koval <dmytrokovalskyi@yahoo.com>
2020-10-06 21:07:12 +0300

    fixLogic

--- src/redux/notifications/notificationsReducer.js
+++ src/redux/notifications/notificationsReducer.js
@@ -1,7 +1,15 @@
 import { createReducer } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
 import notificationsActions from './notificationsActions';
 
-export default createReducer(0, {
+const count = createReducer(0, {
   [notificationsActions.addNotification]: state => state + 1,
   [notificationsActions.removeNotification]: state => state - 1,
+});
+const setNotification = createReducer(false, {
+  [notificationsActions.removeNotification]: () => true,
+});
+export default combineReducers({
+  count,
+  setNotification,
 });
