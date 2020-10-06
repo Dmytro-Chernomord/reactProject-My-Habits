33cc50af3ce4f0ac68f0087e2ee754aa747d54e8
Dmytro <tschernomord@gmail.com>
2020-10-06 11:53:25 +0200

    Revert "Merge pull request #97 from Dmytro-Chernomord/subscribtionConnect"
    This reverts commit 4f11796e0984052d78ec6e95cf65e46eb746d5c0, reversing
    changes made to 3d462dae3d12bf1af4347a50e371a2d9ae77651c.

--- src/Views/PrivateViews/ProfilePage/ProfilePage.js
+++ src/Views/PrivateViews/ProfilePage/ProfilePage.js
@@ -12,7 +12,6 @@ import userSelector from '../../../redux/user/userSelector';
 import Subscriptions from '../SubscriptionsViews/Subscriptions';
 import ChangePassword from '../../../components/ChangePassword/ChangePassword';
 import ProfileForm from '../../../components/ProfileForm/ProfileForm';
-import subCss from '../../PrivateViews/SubscriptionsViews/Subscriptions.module.css';
 
 function ProfilePage({ match, location, toggleModal, changeLayout }) {
   const [showAvatars, setShowAvatars] = useState(false);
@@ -34,27 +33,7 @@ function ProfilePage({ match, location, toggleModal, changeLayout }) {
       setShowSubscriptions(false);
     }
   }, [location.pathname]);
-  const changeColor = () => {
-    switch (subscription) {
-      case 'Noob':
-        return subCss.Noob;
 
-      case 'Basic':
-        return subCss.Basic;
-
-      case 'Standart':
-        return subCss.Standart;
-
-      case 'Premium':
-        return subCss.Premium;
-
-      case 'Ultra':
-        return subCss.Ultra;
-
-      default:
-        return subCss.styleSubscpt;
-    }
-  };
   return (
     <>
       {showSubscriptions ? (
@@ -106,6 +85,7 @@ function ProfilePage({ match, location, toggleModal, changeLayout }) {
 
                   <div className={styles.boxAvatar}>
                     <AvatarUser width="108" />
+
                     <NavLink
                       exact
                       to={`${match.url}/avatars`}
@@ -114,9 +94,12 @@ function ProfilePage({ match, location, toggleModal, changeLayout }) {
                     >
                       Выбрать другой аватар
                     </NavLink>
-                    <div className={styles.subDiv}>
-                      <p className={changeColor()}>{subscription}</p>
-                    </div>
+
+                    {subscription === '' ? (
+                      <p className={styles.typeSubscription}>Basic</p>
+                    ) : (
+                      <p className={styles.typeSubscription}>{subscription}</p>
+                    )}
 
                     <NavLink
                       exact
