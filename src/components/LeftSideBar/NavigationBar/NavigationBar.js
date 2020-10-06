0d818102766d3c7a194372ae9b2f2366133af14b
Nicholas Trunov <anteycu@gmail.com>
2020-10-06 16:13:44 +0300

    add nav activeStyle logic

--- src/components/LeftSideBar/NavigationBar/NavigationBar.js
+++ src/components/LeftSideBar/NavigationBar/NavigationBar.js
@@ -1,31 +1,61 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-// import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { ReactComponent as Cup } from '../../../images/menu-icons/cup.svg';
 import { ReactComponent as Bell } from '../../../images/menu-icons/bell.svg';
 import { ReactComponent as Calendar } from '../../../images/menu-icons/calendar.svg';
-// import calendar from '../../../images/menu-icons/calendar.svg';
 import styles from './NavigationBar.module.css';
 
 const Navigation = ({ match }) => {
-  // console.log(match);
   return (
     <nav className={styles.container}>
       <ul className={styles.navList}>
         <li className={styles.linkBox}>
-          <Link className={styles.iconBox} to={`${match.url}`}>
+          <NavLink
+            className={styles.iconBox}
+            activeStyle={{
+              backgroundColor: '#333333',
+            }}
+            isActive={(_, location) => {
+              if (location.pathname === `${match.url}`) {
+                return true;
+              }
+            }}
+            to={`${match.url}`}
+          >
             <Calendar />
-          </Link>
+          </NavLink>
         </li>
         <li className={styles.linkBox}>
-          <Link className={styles.iconBox} to={`${match.url}/Achievments`}>
+          <NavLink
+            className={styles.iconBox}
+            activeStyle={{
+              backgroundColor: '#333333',
+            }}
+            isActive={(_, location) => {
+              if (location.pathname === `${match.url}/Achievments`) {
+                return true;
+              }
+            }}
+            to={`${match.url}/Achievments`}
+          >
             <Cup />
-          </Link>
+          </NavLink>
         </li>
         <li className={styles.linkBox}>
-          <Link className={styles.iconBox} to={`${match.url}/Notifications`}>
+          <NavLink
+            className={styles.iconBox}
+            activeStyle={{
+              backgroundColor: '#333333',
+            }}
+            isActive={(_, location) => {
+              if (location.pathname === `${match.url}/Notifications`) {
+                return true;
+              }
+            }}
+            to={`${match.url}/Notifications`}
+          >
             <Bell />
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </nav>
