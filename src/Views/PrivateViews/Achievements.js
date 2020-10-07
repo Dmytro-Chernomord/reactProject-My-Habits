a0495af9d49636b0dfa9b7f238d663a8b6f0772a
Koval <dmytrokovalskyi@yahoo.com>
2020-10-07 13:09:03 +0300

    styles edited

--- src/Views/PrivateViews/Achievements.js
+++ src/Views/PrivateViews/Achievements.js
@@ -6,7 +6,6 @@ import Header from '../../components/Header/Header';
 export default function Achievments() {
   return (
     <div className={styles.container}>
-      <Header title="Достижения" />
       {/* <header className={styles.header}>
         <h2 className={styles.title}>Достижения</h2>
         <p>Счет:2025</p>
--- src/components/AchievmentsList/AchievmentsList.js
+++ src/components/AchievmentsList/AchievmentsList.js
@@ -23,110 +23,145 @@ const AchievmentsList = function () {
   console.log(cigarettesData);
   // console.log(daysDontSmoke.length > 0);
   return (
-    <ul className={styles.list}>
-      <li
-        className={
-          daysDontSmoke.length > 0 || oneCigaretteLess
-            ? styles.item_success
-            : styles.item
-        }
-      >
-        <p className={styles.text}>Отказ от одной сигареты</p>
-      </li>
-      <li
-        className={
-          daysDontSmoke.length > 0 || threeCigarettesLess
-            ? styles.item_success
-            : styles.item
-        }
-      >
-        <p className={styles.text}>Отказ от 3 сигарет</p>
-      </li>
-      <li
-        className={
-          daysDontSmoke.length > 0 || fiveCigarettesLess
-            ? styles.item_success
-            : styles.item
-        }
-      >
-        <p className={styles.text}>Отказ от 5 сигарет</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 0 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>Не курю 1 день</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 2 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>Не курю 3 дня</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 4 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>Не курю одну неделю</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 4 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>Не курю 2 недели</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 4 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>Не курю 1 месяц</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 4 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>Не курю 3 месяца</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 4 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>Не курю 6 месяцев</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 4 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>Не курю 1 год</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 4 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>+1 год без сигарет</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 4 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>3 года без сигарет</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 4 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>Уже 5. Дай пять!</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 4 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>Вперед к мечте!</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 4 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>Сохранил 1 час</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 4 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>Сохранил 3 часа</p>
-      </li>
-      <li
-        className={daysDontSmoke.length > 4 ? styles.item_success : styles.item}
-      >
-        <p className={styles.text}>Сохранил 5 часов</p>
-      </li>
-    </ul>
+    <div>
+      <header className={styles.header}>
+        <h2 className={styles.title}>Достижения</h2>
+      </header>
+      <ul className={styles.list}>
+        <li
+          className={
+            daysDontSmoke.length > 0 || oneCigaretteLess
+              ? styles.item_success
+              : styles.item
+          }
+        >
+          <p className={styles.text}>Отказ от одной сигареты</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 0 || threeCigarettesLess
+              ? styles.item_success
+              : styles.item
+          }
+        >
+          <p className={styles.text}>Отказ от 3 сигарет</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 0 || fiveCigarettesLess
+              ? styles.item_success
+              : styles.item
+          }
+        >
+          <p className={styles.text}>Отказ от 5 сигарет</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 0 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>Не курю 1 день</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 2 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>Не курю 3 дня</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 4 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>Не курю одну неделю</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 4 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>Не курю 2 недели</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 4 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>Не курю 1 месяц</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 4 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>Не курю 3 месяца</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 4 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>Не курю 6 месяцев</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 4 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>Не курю 1 год</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 4 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>+1 год без сигарет</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 4 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>3 года без сигарет</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 4 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>Уже 5. Дай пять!</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 4 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>Вперед к мечте!</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 4 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>Сохранил 1 час</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 4 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>Сохранил 3 часа</p>
+        </li>
+        <li
+          className={
+            daysDontSmoke.length > 4 ? styles.item_success : styles.item
+          }
+        >
+          <p className={styles.text}>Сохранил 5 часов</p>
+        </li>
+      </ul>
+    </div>
   );
 };
 
